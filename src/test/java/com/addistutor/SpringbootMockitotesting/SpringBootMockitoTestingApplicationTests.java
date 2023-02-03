2e62664852183aeadbf8305c156b2d40565fc382
haile21 <hailishmit@gmail.com>
2023-02-03 13:19:36 +0300

    getUsers service method tested

--- src/test/java/com/addistutor/SpringbootMockitotesting/SpringBootMockitoTestingApplicationTests.java
+++ src/test/java/com/addistutor/SpringbootMockitotesting/SpringBootMockitoTestingApplicationTests.java
@@ -1,13 +1,35 @@
 package com.addistutor.SpringbootMockitotesting;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.addistutor.SpringbootMockitotesting.dao.UserRepository;
+import com.addistutor.SpringbootMockitotesting.model.User;
+import com.addistutor.SpringbootMockitotesting.service.UserService;
+
 
 @SpringBootTest
 class SpringBootMockitoTestingApplicationTests {
 
+	@Autowired
+	private UserService service;
+
+	@MockBean
+	private UserRepository repository;
+
 	@Test
-	void contextLoads() {
+	public void getUsersTest() {
+		when(repository.findAll()).thenReturn(Stream
+				.of(new User( "Henno", 4, "USA"), new User( "Ezana", 3, "UK")).collect(Collectors.toList()));
+		assertEquals(2, service.getUsers().size());
 	}
 
 }
